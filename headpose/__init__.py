a440c9df687970b6608f8c07b2b53881ee5679a0
OleBialas <bialas.ole@gmx.de>
2021-02-10 14:13:22 +0100

    fix import error

--- headpose/__init__.py
+++ headpose/__init__.py
@@ -2,7 +2,7 @@ import pathlib
 import sys
 __version__ = '0.1'
 
-sys.path.append('../..\\')
+sys.path.append('..\\')
 DIR = pathlib.Path(__file__).parent.resolve()
 
 from headpose.headpose import PoseEstimator
