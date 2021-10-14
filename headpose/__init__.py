63cf6987379d9bc04330f48d906159e14ba3276a
ole <bialas.ole@gmx.de>
2021-10-14 11:01:51 +0200

    import PoseEstimator in init

--- headpose/__init__.py
+++ headpose/__init__.py
@@ -1,6 +1,7 @@
 import pathlib
 import sys
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 sys.path.append('..\\')
+from headpose.detect import PoseEstimator
 DIR = pathlib.Path(__file__).parent.resolve()
